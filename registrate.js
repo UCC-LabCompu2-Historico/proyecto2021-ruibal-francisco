95b4ef1d9ed800e3ccc07ae4a53f39e1e19de352
FranciscoRuibal <80827588+FranciscoRuibal@users.noreply.github.com>
2022-02-07 19:24:39 -0300

    Agrego cronometro y tabla mejores puntajes, canvas me aparece en blanco

--- registrate.js
+++ registrate.js
@@ -1,3 +1,9 @@
+/* Guarda los valores en localStorage
+* @method SignUp
+* @param formData
+* @return devuelve los valores por consola
+*/
+
 const signUp = e => {
     let formData= {
         usuario: document.getElementById('usuario').value,
@@ -7,4 +13,14 @@ const signUp = e => {
     localStorage.setItem('formData', JSON.stringify(formData));
     console.log(localStorage.getItem('formData'));
     e.preventDefault();
+}
+
+/* Guarda los valores en localStorage
+* @method caragarPuntaje
+*/
+function cargarPuntaje(){
+    let tiempoActual = localStorage.getItem("tiempoActual");
+    let nombreJugadorActual = localStorage.getItem("NombreDelUsuario");
+    document.getElementById("nombreJug").innerHTML = nombreJugadorActual;
+    document.getElementById("jugTiempoAct").innerHTML = tiempoActual;
 }
